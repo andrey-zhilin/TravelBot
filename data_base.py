933d4a1d286449291ff4ddad3a4f660b1905062f
Andrey Zhilin <forproskater82@gmail.com>
2020-09-08 17:03:27 +0300

    get data from api

--- data_base.py
+++ data_base.py
@@ -1,2 +1,53 @@
 import sqlite3
+import requests
+from pprint import pprint
+import os
 
+
+
+
+def api_query():
+    country_list = []
+    country_request = requests.get('http://api.travelpayouts.com/data/ru/countries.json')
+    for country in country_request.json():
+        country_list.append((country['code'],country['name'],country['currency'], \
+            country['name_translations']['en']))
+    #pprint(country_list)
+
+    city_list = []
+    city_request = requests.get('http://api.travelpayouts.com/data/ru/cities.json')
+    for city in city_request.json():
+        
+        city_list.append((city['code'],city['name'], \
+                 city['coordinates']['lon']  if (city.get('coordinates') is not None) else None, \
+                     city['coordinates']['lat']  if (city.get('coordinates') is not None) else None, \
+                      city['name_translations']['en'],city['country_code']))
+    #pprint(city_list)
+    trasport_point_list =[]
+    trasport_point_request = requests.get('http://api.travelpayouts.com/data/ru/airports.json')
+    for point in trasport_point_request.json():
+        trasport_point_list.append((point['code'],point['name'], \
+            point['iata_type'],point['flightable'],point['name_translations']['en'], \
+            point['city_code']))
+        
+    #pprint(trasport_point_list)
+
+
+
+
+def database():
+    db_filename = 'transport.db'
+    schema_filename = 'scheme.sql'
+
+    db_exists = os.path.exists(db_filename)
+    conn = sqlite3.connect(db_filename)
+   
+    if not db_exists:
+        print('Creating schema...')
+        with open(schema_filename, 'r') as f:
+            schema = f.read()
+        conn.executescript(schema)
+        print('Done')
+
+#api_query()
+database()
--- scheme.sql
+++ scheme.sql
@@ -1,23 +1,27 @@
 CREATE TABLE IF NOT EXISTS country(
 country_id  VARCHAR NOT NULL PRIMARY KEY,
-name_primary VARCHAR NOT NULL,
+name_primary VARCHAR ,
+currency VARCHAR,
 name_en VARCHAR,
-currency VARCHAR
 );
 
 CREATE TABLE IF  NOT EXISTS city(
 city_id VARCHAR NOT NULL PRIMARY KEY,
-name_primary VARCHAR NOT NULL,
-name_en VARCHAR,
+name_primary VARCHAR,
 coordinates_lon VARCHAR,
 coordinates_lat VARCHAR,
+name_en VARCHAR,
+country_code VARCHAR,
 FOREIGN KEY(country_code) REFERENCES country(country_id)
 );
 
 CREATE TABLE IF  NOT EXISTS transport_point(
 point_id VARCHAR NOT NULL PRIMARY KEY,
-name_primary VARCHAR NOT NULL,
+name_primary VARCHAR,
+iata_type VARCHAR,
+flightable BOOLEAN,
 name_en VARCHAR,
+city_code VARCHAR,
 FOREIGN KEY(city_code) REFERENCES city(city_id)
 );
 
