6c5498819054fcb9883bcc89d7928e814797dc76
Андрей Жилин <zhilin.a@hub.sknt.ru>
2020-10-01 15:33:38 +0300

    add sql query

--- class_query.py
+++ class_query.py
@@ -17,7 +17,7 @@ class query():
             f'departure_tp.name_primary, arrival_tp.name_primary, '\
                 f'departure_city.name_primary, arrival_city.name_primary, ' \
                     f'departure_coutry.name_primary, arrival_country.name_primary, ' \
-                        f'departure_tp.city_code, arrival_tp.city_code FROM routes r ' \
+                        f'departure_tp.city_code, arrival_tp.city_code, arrival_city.coordinates_lon, arrival_city.coordinates_lat FROM routes r ' \
                             f'INNER JOIN transport_point departure_tp ON r.departure_airport_iata = departure_tp.point_id ' \
                                 f'INNER JOIN transport_point arrival_tp ON r.arrival_airport_iata = arrival_tp.point_id ' \
                                     f'INNER JOIN city departure_city on departure_tp.city_code = departure_city.city_id ' \
@@ -32,15 +32,15 @@ class query():
         self.response_dict = {'departure_airport_iata':response[0], 'arrival_airport_iata':response[1], 'departure_airport_name':response[2], 
             'arrival_airport_name':response[3],'departure_city_name':response[4], 'arrival_city_name':response[5],'departure_country_name':response[6], 
                 'arrival_country_name':response[7] } # в этом словаре будут содержаться данные для выдачи фронту
-        self.meta_dict = {'departure_city_code':response[8], 'arrival_city_code':response[9]} # нужен для передачи значения в функцию по поиску билетов
+        self.meta_dict = {'departure_city_code':response[8], 'arrival_city_code':response[9], 'arrival_city_lon':response[10], 'arrival_city_lat':response[11] } # нужен для передачи значения в функцию по поиску билетов
         return self.meta_dict
 
     def tickets(self, departure_city_code, arrival_city_code, token=api_token.token_travel):  # IATA код города 
         """"Ищет дешёвые билеты по выбранному направлению. Записывает данные в словарь для выдачи фронату"""
         headers = {'X-Access-Token':token,
         'Accept-Encoding':'gzip, deflate'}
-        url = f'http://api.travelpayouts.com/v1/prices/cheap?origin={departure_city_code}&destination={arrival_city_code}'    
-        
+        #url = f'http://api.travelpayouts.com/v1/prices/cheap?origin={departure_city_code}&destination={arrival_city_code}'    
+        url = f'http://api.travelpayouts.com/v1/prices/direct?origin={departure_city_code}&destination={arrival_city_code}'
         try:
             response = requests.request("GET",url,headers=headers,timeout=1)
             print(url+'&token='+token)
@@ -62,7 +62,7 @@ class query():
                 print("Unsuccessful response. Reason:", response.json()['error'] )
         except requests.exceptions.RequestException:
             print('Ошибка при загрузке данных')
-
+    
     def run(self):
         self.tickets(**self.choosing_random_route(self.origin_city))
         
